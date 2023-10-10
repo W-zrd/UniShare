046192db079c9571d13ab7ef01d2f6de470fb8fa
Glorious Satria Dhamang Aji <glorioussatria@gmail.com>
2023-10-10 21:04:37 +0700

    init login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
+});
+
+Route::controller(AuthController::class)->group(function () {
+    Route::get('/login', 'showLoginForm')->name('login');
+    Route::post('/login', 'login');
+    Route::get('/register', 'showRegisterForm')->name('register');
+    Route::post('/register', 'register');
+    Route::post('/logout', 'logout')->name('logout');
+});
+
+Route::controller(UserController::class)->group(function(){
+    Route::get('/users', 'index')->name('users');
+    Route::get('/users/create', 'create')->name('users.create');
+    Route::post('/users', 'store')->name('users.store');
+    Route::get('/users/{id}', 'show')->name('users.show');
+    Route::get('/users/{id}/edit', 'edit')->name('users.edit');
+    Route::put('/users/{id}', 'update')->name('users.update');
+    Route::delete('/users/{id}', 'destroy')->name('users.destroy');
 });
