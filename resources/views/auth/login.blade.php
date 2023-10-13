12dbc091302ebea92e81b9432066877fd046f077
rasyeeed <muhammadnurrasyid@gmail.com>
2023-10-13 16:33:19 +0700

    login fixes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,7 +44,7 @@
                             <a class="nav-link" href="/register">Register</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/login">Login</a>
+                            <a class="nav-link active" aria-current="page" href="/login" style="color: #f75600">Login</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Home</a>
@@ -64,13 +64,13 @@
         <div class="row m-2 justify-content-center">
             <div class="col-7 mt-5">
                 <div class="content-box">
-                    <h1 class="fw-bold">Login to UniShare.</h1>
+                    <h1 class="fw-bold" style="font-size:65px">Login to UniShare.</h1>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
                         <div class="mb-3">
                             <label for="username" class="form-label">Username</label>
                             <input type="text" class="form-control" id="username" name="username" aria-describedby="usernameHelp" required>
-                            <div id="usernameHelp" class="form-text">We'll never share your username with anyone else.</div>
+                            <div id="usernameHelp" class="form-text" >We'll never share your username with anyone else.</div>
                         </div>
                         <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
@@ -88,53 +88,6 @@
             <!-- IMAGE -->
             <div class="col-5">
                 <img src="/assets/img/book2.png" class="img-fluid p-0" alt="s">
-            </div>
-        </div>
-    </section>
-
-    <!-- WAVES -->
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path
-            fill="#121415"
-            fill-opacity="1"
-            d="M0,160L24,144C48,128,96,96,144,85.3C192,75,240,85,288,117.3C336,149,384,203,432,229.3C480,256,528,256,576,240C624,224,672,192,720,160C768,128,816,96,864,106.7C912,117,960,171,1008,197.3C1056,224,1104,224,1152,224C1200,224,1248,224,1296,208C1344,192,1392,160,1416,144L1440,128L1440,0L1416,0C1392,0,1344,0,1296,0C1248,0,1200,0,1152,0C1104,0,1056,0,1008,0C960,0,912,0,864,0C816,0,768,0,720,0C672,0,624,0,576,0C528,0,480,0,432,0C384,0,336,0,288,0C240,0,192,0,144,0C96,0,48,0,24,0L0,0Z"
-        ></path>
-    </svg>
-
-    <!-- ABOUT -->
-    <section class="about">
-        <div class="container text-center">
-            <div class="row">
-                <div class="col-12">
-                    <h3 class="text-center fs-2 fw-semibold">Features</h3>
-                </div>
-            </div>
-
-            <div class="row m-4 text-start">
-                <!-- CONTENT 1-->
-                <div class="col-md">
-                    <div class="shadow p-4 mb-5 rounded mx-auto">
-                        <img src="/assets/img/study.png" class="img-thumb" alt="...">
-                        <h5 class="mt-3">Beasiswa</h5>
-                        <p class="pt-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, doloremque. Assumenda excepturi at reprehenderit dignissimos?</p>
-                    </div>
-                </div>
-                <!-- CONTENT 2-->
-                <div class="col-md">
-                    <div class="shadow p-4 mb-5 rounded mx-auto">
-                    <img src="/assets/img/loker.png" class="img-thumb" alt="...">
-                        <h5 class="mt-3">Lowongan Kerja</h5>
-                        <p class="pt-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, doloremque. Assumenda excepturi at reprehenderit dignissimos?.</p>
-                    </div>
-                </div>
-                <!-- CONTENT 3-->
-                <div class="col-md">
-                    <div class="shadow p-4 mb-5 rounded mx-auto">
-                    <img src="/assets/img/clock.png" class="img-thumb" alt="...">
-                     <h5 class="mt-3">Progress Tracker</h5>
-                     <p class="pt-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, doloremque. Assumenda excepturi at reprehenderit dignissimos?</p>
-                    </div>
-                </div>
             </div>
         </div>
     </section>
