e94ce74367357c8e6f1013660e0a7b0c2d1c61c4
KuRynnn <fahryan107@gmail.com>
2023-10-29 18:03:32 +0700

    Update Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -72,7 +72,7 @@
         <div class="row">
             <div class="col-lg-6">
                 <div class="card">
-                    <div class="card-body">
+                    <div class="card-body card-atas">
                         <h4 class="card-title">Temukan peluang yang tepat untuk mewujudkan impianmu bersama UniShare!</h4>
                         <div class="container mt-4">
                             <div class="input-group">
@@ -86,15 +86,15 @@
                 </div>
             </div>
             <div class="col-lg-3">
-                <div class="card d-flex justify-content-center align-items-center">
+                <div class="card d-flex justify-content-center align-items-center card-atas">
                     <img class="card-img-top img-fluid" src="assets/img/Bullseye.png" alt="Card image cap" style="width: 200px;">
                     <div class="card-body">
-                        <h5 class="card-title">Capaian Belajar</h5>
+                        <h5 class="card-title ">Capaian Belajar</h5>
                     </div>
                 </div>
             </div>
             <div class="col-lg-3">
-                <div class="card d-flex justify-content-center align-items-center">
+                <div class="card d-flex justify-content-center align-items-center card-atas">
                     <img class="card-img-top img-fluid" src="assets/img/Bullseye.png" alt="Card image cap" style="width: 200px;">
                     <div class="card-body">
                         <h5 class="card-title">Capaian Belajar</h5>
@@ -118,7 +118,7 @@
             <div class="col-lg-3">
                 <div class="card h-100 d-flex align-items-center">
                     <svg class="bd-placeholder-img flex-fill" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
-                    <div class="card-body flex-fill">
+                    <div class="card-body flex-fill card-bawah">
                         <h5 class="card-title">Nama Acara [Max 2 Baris]</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
@@ -127,7 +127,7 @@
             <div class="col-lg-3">
                 <div class="card h-100 d-flex align-items-center">
                     <svg class="bd-placeholder-img flex-fill" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
-                    <div class="card-body flex-fill">
+                    <div class="card-body flex-fill card-bawah">
                         <h5 class="card-title">Nama Acara [Max 2 Baris]</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
@@ -136,7 +136,7 @@
             <div class="col-lg-3">
                 <div class="card h-100 d-flex align-items-center">
                     <svg class="bd-placeholder-img flex-fill" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
-                    <div class="card-body flex-fill">
+                    <div class="card-body flex-fill card-bawah">
                         <h5 class="card-title">Nama Acara [Max 2 Baris]</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
@@ -145,7 +145,7 @@
             <div class="col-lg-3">
                 <div class="card h-100 d-flex align-items-center">
                     <svg class="bd-placeholder-img flex-fill" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
-                    <div class="card-body flex-fill">
+                    <div class="card-body flex-fill card-bawah">
                         <h5 class="card-title">Nama Acara [Max 2 Baris]</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
