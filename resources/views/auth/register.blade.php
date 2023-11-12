c7dd9e3722fba3fc3ab67bce14c8569966c5f892
Glorious Satria Dhamang Aji <glorioussatria@gmail.com>
2023-11-12 21:53:33 +0700

    afkh

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.generics')
-
+    @section('content')
             <div class="container-fluid mt-3">
                 <div class="row justify-content-center">                             
                     <div class="row m-10 justify-content-center">
@@ -40,14 +40,7 @@
                         <div class="col-4">
                             <img src="/assets/img/UniShare-with-Text-2.png" class="img-fluid p-0" alt="s" style="transform:scale(0.8)">
                         </div>
-                    </div>
-
-                    <!-- JAVASCRIPT -->
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-                    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-                    <script src="assets/js/"></script>  
+                    </div> 
                 </div>
             </div>
-        </section>
-    </body>
-</html>
+    @endsection
